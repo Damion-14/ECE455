c9299c62095cab80504269f42913fdd35042c9e1
Damion Huppert <damionhuppert@Damions-MacBook-Pro.local>
2025-09-19 15:20:37 -0500

    Problem 1

---
+++ HW02/hello_threads.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <thread>
+#include <vector>
+
+void say_hello(int thread, int total_threads) {
+  printf("Hello from thread %i of %i", thread, total_threads);
+}
+
+int main() {
+
+  const int total_threads = 5;
+  std::vector<std::thread> threads;
+  threads.reserve (total_threads) ;
+
+  for (int thread = 0; thread < total_threads; thread++) {
+    threads.emplace_back(say_hello , thread , total_threads );
+  }
+
+  for ( auto & t : threads ) t . join () ;
+  
+  return 0;
+}
