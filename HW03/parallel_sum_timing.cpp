72a6b39149a038950d4d10e1f39efda48fb5ce26
Damion Huppert <damionhuppert@Damions-MacBook-Pro.local>
2025-09-26 16:00:18 -0500

    HW03

---
+++ HW03/parallel_sum_timing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iterator>
+#include <omp.h>
+#include <ostream>
+#include <vector>
+
+int main() {
+  const int N = 10000000;
+  std::vector<int> data(N, 1); // all ones
+
+  for (int thread = 1; thread < 9; thread *= 2) {
+    double sum = 0;
+
+    double t0 = omp_get_wtime();
+#pragma omp parallel for reduction(+ : sum) num_threads(threads)
+    for (int i = 0; i < N; ++i) {
+      sum += data[i];
+    }
+    double t1 = omp_get_wtime();
+    std::cout << "Thread: " << thread << std::endl << "Time: "  << t1-t0 << std::endl << "Sum: " << sum << std::endl;
+  }
+  return 0;
+}
