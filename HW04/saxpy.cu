dc519eb4d4ce81ce7b22faddb09b0e915407a1d9
Damion Huppert <damionhuppert@Damions-MacBook-Pro.local>
2025-10-03 16:14:57 -0500

    HW04

--- HW04/saxpy.cu
+++ HW04/saxpy.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void vector_add(int n, float a, float *x, float *y) {
+__global__ void saxpy(int n, float a, float *x, float *y) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < n) {
         y[i] = a * x[i] + y[i];
